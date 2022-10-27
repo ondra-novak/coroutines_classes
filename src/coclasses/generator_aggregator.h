11e78159e09614ebcd97515b4b4cef811482fd62
Ondřej Novák <ondra-novak@email.cz>
2022-10-27 15:31:17 +0200

    workaround gcc internal compiler error

--- src/coclasses/generator_aggregator.h
+++ src/coclasses/generator_aggregator.h
@@ -6,6 +6,57 @@
 
 namespace cocls {
 
+namespace _details {
+
+template<typename T>
+struct generator_aggregator_callback {
+        using Queue = queue<std::pair<generator<T> *, std::optional<T> > >;
+        generator_aggregator_callback(generator<T> &gen, Queue &queue, std::exception_ptr &exp)
+            :_gen(gen),_queue(queue),_exp(exp) {}
+        generator_aggregator_callback(const generator_aggregator_callback &) = default;
+        generator_aggregator_callback &operator=(const generator_aggregator_callback &) = delete;
+        void operator()(std::optional<T> &&val) {
+            std::exception_ptr n = std::current_exception();
+            if (n) _exp = n;            
+            _queue.push({&_gen, std::move(val)});
+        }
+        generator<T> &_gen;
+        Queue &_queue;
+        std::exception_ptr &_exp;
+};
+
+
+//tracks count of active generators, handles final destruction
+/* Because generators can be async and during destruction, they can
+ * be still pending. So before we can destroy them we must
+ * wait for finish. This is blocking operation!
+ * 
+ * cleanup is easy, just flush whole queue synchronously
+ */
+template<typename T>
+struct generator_aggregator_controller {
+    using Queue = queue<std::pair<generator<T> *, std::optional<T> > >;
+    generator_aggregator_controller(std::size_t count, Queue &queue):_count(count),_queue(queue) {}
+    generator_aggregator_controller(const generator_aggregator_controller  &) = delete;
+    generator_aggregator_controller &operator=(const generator_aggregator_controller  &) = delete;
+    ~generator_aggregator_controller() {
+        //there is always 1 less value, than count active of generators
+        //because only place when you can delete generator_aggregator
+        //is during co_yield. And because we are in co_yield, one
+        //of generators is not active
+        while (_count>1) { 
+            _queue.pop().wait();
+            _count--;
+        }
+    }
+    operator bool() const {return _count>0;}
+    void fin() {_count--;}
+    
+    std::size_t _count;
+    Queue &_queue;
+};
+
+}
 ///Aggregator of multiple generators
 /**
  * @param list__ list of generators to aggregate. The vector is passed 
@@ -19,49 +70,9 @@ generator<T> generator_aggregator(std::vector<generator<T> > list__) {
     std::exception_ptr exp;
     
     using Queue = queue<std::pair<generator<T> *, std::optional<T> > >; 
+    using cb = _details::generator_aggregator_callback<T>;
+    using controller = _details::generator_aggregator_controller<T>;
     
-    struct cb {
-        cb(generator<T> &gen, Queue &queue, std::exception_ptr &exp)
-            :_gen(gen),_queue(queue),_exp(exp) {}
-        cb(const cb &) = default;
-        cb &operator=(const cb &) = delete;
-        void operator()(std::optional<T> &&val) {
-            std::exception_ptr n = std::current_exception();
-            if (n) _exp = n;            
-            _queue.push({&_gen, std::move(val)});
-        }
-        generator<T> &_gen;
-        Queue &_queue;
-        std::exception_ptr &_exp;
-    };
-    
-    //tracks count of active generators, handles final destruction
-    /* Because generators can be async and during destruction, they can
-     * be still pending. So before we can destroy them we must
-     * wait for finish. This is blocking operation!
-     * 
-     * cleanup is easy, just flush whole queue synchronously
-     */
-    struct controller {
-        controller(std::size_t count, Queue &queue):_count(count),_queue(queue) {}
-        controller(const controller  &) = delete;
-        controller &operator=(const controller  &) = delete;
-        ~controller() {
-            //there is always 1 less value, than count active of generators
-            //because only place when you can delete generator_aggregator
-            //is during co_yield. And because we are in co_yield, one
-            //of generators is not active
-            while (_count>1) { 
-                _queue.pop().wait();
-                _count--;
-            }
-        }
-        operator bool() const {return _count>0;}
-        void fin() {_count--;}
-        
-        std::size_t _count;
-        Queue &_queue;
-    };
     
     std::vector<generator<T> > list(std::move(list__));
     Queue queue;
